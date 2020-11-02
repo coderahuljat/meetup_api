4dd68c388149c21a10bf33dd1eadadd0f19ed20e
Nirmeet Sanghani lp <nirmeets@gmail.com>
2020-11-02 23:09:55 +0530

    Added database migration for participants

---
+++ app/Http/Controllers/ParticipantController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Participant;
+use Illuminate\Http\Request;
+use Response;
+
+class ParticipantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $participant = Participant::paginate(10);
+        if($participant)
+        {
+          $data = [
+                    'status'          => 200,
+                    'payload'         => $participant,
+                    'message'         => 'Success'
+                  ];
+          return $response = Response::json($data, 200);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Participant  $participant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Participant $participant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Participant  $participant
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Participant $participant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Participant  $participant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Participant $participant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Participant  $participant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Participant $participant)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_11_02_085411_create_participants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->decimal('age',3,2);
+            $table->date('dob');
+            $table->string('profession');
+            $table->integer('no_of_guests');
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
